b273af2ff0777f55096c263c61199a5bf8b1af77
acrdiaz <acrdiaz@msn.com>
2025-02-15 17:41:36 -0400

    add static repository for categories

---
+++ CSharpNET8CoreMVC/Models/CategoriesRepository.cs
@@ -0,0 +1,67 @@
+﻿namespace CSharpNET8CoreMVC.Models
+{
+    public static class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>
+        {
+            new Category { Id = 1, Name = "Beverages", Description = "Soft drinks, coffees, teas, beers, and ales" },
+            new Category { Id = 2, Name = "Condiments", Description = "Sweet and savory sauces, relishes, spreads, and seasonings" },
+            new Category { Id = 3, Name = "Confections", Description = "Desserts, candies, and sweet breads" },
+            new Category { Id = 4, Name = "Dairy Products", Description = "Cheeses" },
+            new Category { Id = 5, Name = "Grains/Cereals", Description = "Breads, crackers, pasta, and cereal" },
+            new Category { Id = 6, Name = "Meat/Poultry", Description = "Prepared meats" },
+            new Category { Id = 7, Name = "Produce", Description = "Dried fruit and bean curd" },
+            new Category { Id = 8, Name = "Seafood", Description = "Seaweed and fish" }
+        };
+
+        //public static IEnumerable<Category> Categories => categories;
+
+        public static void AddCategory(Category category)
+        {
+            var maxId = _categories.Max(c => c.Id);
+            category.Id = maxId + 1;
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategoryById(int id)
+        {
+            // => _categories.FirstOrDefault(c => c.Id == id);
+            
+            var category = _categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return new Category
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    Description = category.Description,
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int id, Category category)
+        {
+            if (id != category.Id) return;
+
+            var categoryToUpdate = GetCategoryById(id);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int id)
+        {
+            var category = GetCategoryById(id);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
